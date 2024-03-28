a266f049367c467d6b2c89c2333329fb30a4bd52
Ola Munthe Vassbotn <olamva@stud.ntnu.no>
2024-03-28 22:49:22 +0100

    add: Metadata

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,7 +1,14 @@
 import "@/app/globals.css";
 import ApplicationShell from "@/components/default/ApplicationShell";
+import { Metadata } from "next";
 import { NextIntlClientProvider, useMessages } from "next-intl";
 import { ReactNode } from "react";
+
+export const metadata: Metadata = {
+	title: "Ola Munthe Vassbotn's Portfolio",
+	description: "A website designed to show off Ola's skills and projects.",
+};
+
 export default function LocaleLayout({
 	children,
 	params: { locale },
@@ -13,7 +20,6 @@ export default function LocaleLayout({
 	return (
 		<html lang={locale}>
 			<NextIntlClientProvider locale={locale} messages={messages}>
-
 				<ApplicationShell>{children}</ApplicationShell>
 			</NextIntlClientProvider>
 		</html>
