69ef7c609c83675a0fce37e1597e4ee6acdedae7
Ola Munthe Vassbotn <olamva@stud.ntnu.no>
2024-03-28 01:19:38 +0100

    add: Todo flag

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,7 +3,11 @@ import Template from "@/components/default/template";
 export default function Home() {
 	return (
 		<Template>
-			<></>
+			<>
+				{
+					// TODO: Update the content of the page, and if need be change what the page is meant to be.
+				}
+			</>
 		</Template>
 	);
 }
