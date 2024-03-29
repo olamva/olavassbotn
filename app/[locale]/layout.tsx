cdccd2eab206677cfe656bdf61993bc9c72322a3
Ola Munthe Vassbotn <olamva@stud.ntnu.no>
2024-03-29 15:14:10 +0100

    fix: No longer issue with theme persistence Switched from localstorage to cookies

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,8 +1,10 @@
 import "@/app/globals.css";
 import ApplicationShell from "@/components/default/ApplicationShell";
+import { PaletteMode } from "@mui/material";
 import { Metadata } from "next";
 import { NextIntlClientProvider, useMessages } from "next-intl";
 import { unstable_setRequestLocale } from "next-intl/server";
+import { cookies } from "next/headers";
 import { ReactNode } from "react";
 
 export const metadata: Metadata = {
@@ -19,10 +21,16 @@ export default function LocaleLayout({
 }) {
 	unstable_setRequestLocale(locale);
 	const messages = useMessages();
+	const cookieStore = cookies();
+	const savedMode = cookieStore.get("themeMode");
+	const savedTheme =
+		(savedMode?.value as PaletteMode) ?? "light";
 	return (
 		<html lang={locale}>
 			<NextIntlClientProvider locale={locale} messages={messages}>
-				<ApplicationShell>{children}</ApplicationShell>
+				<ApplicationShell savedMode={savedTheme}>
+					{children}
+				</ApplicationShell>
 			</NextIntlClientProvider>
 		</html>
 	);
