0f8bcebe6eb690ee07eff59c41eb98f1dde5070b
Ola Munthe Vassbotn <olamva@stud.ntnu.no>
2024-03-29 15:08:45 +0100

    fix: Not-found page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,9 @@
-import "@/app/globals.css";
-import ApplicationShell from "@/components/default/ApplicationShell";
-import { Metadata } from "next";
-import { NextIntlClientProvider, useMessages } from "next-intl";
-import { unstable_setRequestLocale } from "next-intl/server";
 import { ReactNode } from "react";
 
-export const metadata: Metadata = {
-	title: "Ola Munthe Vassbotn's Portfolio",
-	description: "A website designed to show off Ola's skills and projects.",
+type Props = {
+	children: ReactNode;
 };
 
-export default function RootLayout({ children }: { children: ReactNode }) {
-	const messages = useMessages();
-	unstable_setRequestLocale("en");
-	return (
-		<html lang="en">
-			<NextIntlClientProvider locale="en" messages={messages}>
-				<ApplicationShell>{children}</ApplicationShell>
-			</NextIntlClientProvider>
-		</html>
-	);
+export default function RootLayout({ children }: Props) {
+	return children;
 }
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,39 +1,13 @@
 "use client";
-import { Home } from "@mui/icons-material";
-import { Box, Button, Typography } from "@mui/material";
-import { useTheme } from "@mui/material/styles";
-import { useLocale } from "next-intl";
+
+import Error from "next/error";
 
 export default function NotFound() {
-	const locale = useLocale();
-	const theme = useTheme();
 	return (
-		<Box
-			sx={{
-				display: "flex",
-				flexDirection: "column",
-				alignItems: "center",
-				gap: "2rem",
-				marginTop: "5rem",
-			}}
-		>
-			<Typography variant="h2">404: Page Not Found</Typography>
-			<Typography variant="body1">
-				Could not find requested resource
-			</Typography>
-			<Button
-				href={"/" + locale}
-				size="large"
-				startIcon={
-					<Home sx={{ color: theme.palette.secondary.main }} />
-				}
-				sx={{
-					backgroundColor: theme.palette.primary.main,
-					color: theme.palette.primary.contrastText,
-				}}
-			>
-				Return Home
-			</Button>
-		</Box>
+		<html lang="en">
+			<body>
+				<Error statusCode={404} />
+			</body>
+		</html>
 	);
 }
