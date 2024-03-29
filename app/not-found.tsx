d7c9a8331854f063c1c47cac652a5e51a7d4bf54
Ola Munthe Vassbotn <olamva@stud.ntnu.no>
2024-03-29 15:30:35 +0100

    fix: Finalized not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,29 @@
 "use client";
 
-import Error from "next/error";
+import { Home } from "@mui/icons-material";
+import { Box, Button, Typography } from "@mui/material";
 
 export default function NotFound() {
 	return (
 		<html lang="en">
 			<body>
-				<Error statusCode={404} />
+				<Box
+					sx={{
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+						gap: "2rem",
+						marginTop: "5rem",
+					}}
+				>
+					<Typography variant="h2">404: Page Not Found</Typography>
+					<Typography variant="body1">
+						Could not find requested resource
+					</Typography>
+					<Button href="/" size="large" startIcon={<Home />}>
+						Return Home
+					</Button>
+				</Box>
 			</body>
 		</html>
 	);
