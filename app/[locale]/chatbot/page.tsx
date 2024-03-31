7d92737b03429b51d86ed39c0aeb3378d977ae1c
Ola Munthe Vassbotn <olamva@stud.ntnu.no>
2024-03-31 18:48:52 +0200

    add: Temp chatbot page.tsx

---
+++ app/[locale]/chatbot/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Typography } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+
+export default function ChatBotPage() {
+	const theme = useTheme();
+	return (
+		<Typography
+			variant="h1"
+			align="center"
+			sx={{
+				color: theme.palette.primary.contrastText,
+				fontSize: {
+					xs: "2.5rem",
+					sm: "3rem",
+					md: "4rem",
+					lg: "5rem",
+				},
+			}}
+			gutterBottom
+		>
+			TODO CHATBOT
+		</Typography>
+	);
+}
